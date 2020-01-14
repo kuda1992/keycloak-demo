7fac5c1c338d863ec8b7e7d7103808edf98d260b
kuda <kuda.nyamainashe@ext.soprasteria.com>
2020-01-14 14:50:45 +0000

    config roles

--- src/main/java/com/spass/keycloak/controllers/LibraryController.java
+++ src/main/java/com/spass/keycloak/controllers/LibraryController.java
@@ -3,6 +3,7 @@ package com.spass.keycloak.controllers;
 import com.spass.keycloak.repositories.BookRepository;
 import org.keycloak.KeycloakSecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
